66a1d5f604a7faf22abb7c968d5c5b1a296c3a95
Chris Anderberg <christopher.anderberg@gmail.com>
2015-08-04 21:34:46 -0700

    Add parentheses to make solution 1 more concise

--- 1.hs
+++ 1.hs
@@ -1,3 +1,3 @@
 -- Print the sum of all multiples of 3 and 5 that are less than 1000
 main = print (sum (filter multiple3or5 [1..999]))
-  where multiple3or5 n = n `mod` 3 == 0 || n `mod` 5 == 0
+  where multiple3or5 n = (n `mod` 3 == 0) || (n `mod` 5 == 0)
