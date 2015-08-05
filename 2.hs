0cd7385df18dfe5f7439c338814d47a3a17e61be
Chris Anderberg <christopher.anderberg@gmail.com>
2015-08-04 22:15:44 -0700

    Make solution to problem 2 more concise

--- 2.hs
+++ 2.hs
@@ -1,15 +1,5 @@
 -- print the sum of all even fibonacci numbers less than 4000000
-main = do print (sum (filter even (fibsLessThan 4000000)))
-
--- get all the fibonacci numbers
-fibs :: Integer -> Integer -> [Integer]
-fibs x y = x : (fibs y (x + y))
-
--- get all the fibonacci numbers less than a max value
-fibsLessThan :: Integer -> [Integer]
-fibsLessThan m = numsLessThanR m (fibs 1 1)
-
--- get all numbers less than a max value, assuming list is in accending order
-numsLessThanR :: Integer -> [Integer] -> [Integer]
-numsLessThanR m (h:t) | h < m     = h : (numsLessThanR m t)
-                      | otherwise = []
+main = print (sum (filter even (fibs `lessThan` 4000000)))
+  where lessThan xs n = takeWhile (< n) xs
+        fibs = fib 0 1
+          where fib x y = x : fib y (x + y)
