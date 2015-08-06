0e08cd28b9fb92b58102c07003228c404682d684
Chris Anderberg <christopher.anderberg@gmail.com>
2015-08-05 18:15:44 -0700

    Added type definitions to avoid type ambiguity and warnings

--- 2.hs
+++ 2.hs
@@ -1,5 +1,7 @@
 -- print the sum of all even fibonacci numbers less than 4000000
+main :: IO()
 main = print (sum (filter even (fibs `lessThan` 4000000)))
   where lessThan xs n = takeWhile (< n) xs
         fibs = fib 0 1
-          where fib x y = x : fib y (x + y)
+          where fib :: Integer -> Integer -> [Integer]
+                fib x y = x : fib y (x + y)
