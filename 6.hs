6784037ef0bee10c4fa354bec7279562b789c067
Christopher Anderberg <christopher.anderberg@gmail.com>
2015-08-09 18:28:58 -0700

    Clean solution for problem 6

--- 6.hs
+++ 6.hs
@@ -1,2 +1,3 @@
 -- Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
-main = do print (((sum [1..100]) ^ 2) - (sum (map (\x -> x^2) [1..100])))
+main :: IO()
+main = print $ sum [1..100] ^ 2 - (sum $ map (^2) [1..100])
