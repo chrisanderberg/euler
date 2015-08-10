40118fde3f5768deaf714e9ee2697344c7cdf2e6
Christopher Anderberg <chris@Christophers-MacBook.local>
2015-08-09 17:51:43 -0700

    Simplified and cleaned solution to problem 4

--- 4.hs
+++ 4.hs
@@ -1,19 +1,11 @@
--- print the largest plindromic number that is a product of two 3-digit numbers
+-- print the largest palindromic number that is a product of two 3-digit numbers
 main :: IO()
-main = print $ maximum palindromes
+main = print $ maximum $ palindromes [100..999]
 
--- get all the palindromes of two 3-digit number products
-palindromes :: [Int]
-palindromes  = filter isPalindrome $ map (\(x,y) -> x*y) $ combinationProducer [100..999]
-
--- given a list of elements, produce a new list with every combination of elements
-combinationProducer :: [a] -> [(a,a)]
-combinationProducer = combinationProducerR []
-
--- core recursive function to get each combination of elements
-combinationProducerR :: [a] -> [a] -> [(a,a)]
-combinationProducerR _    []   = []
-combinationProducerR xs (y:ys) = map (\x -> (x,y)) xs ++ combinationProducerR (y:xs) ys
+-- get all palindromes produced by any combination of 2 numbers in the arg
+palindromes :: [Int] -> [Int]
+palindromes   []   = []
+palindromes (x:xs) = (filter isPalindrome $ map (\y -> x * y) xs) ++ palindromes xs
 
 -- given a number, return a list of its digits in reverse order
 -- digitize 184 => [4, 8, 1]
