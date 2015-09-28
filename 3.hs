3e49deba724a63b13ed1fd38073eb7a19000e3ab
Christopher Anderberg <christopher.anderberg@gmail.com>
2015-09-27 18:33:53 -0700

    Solved 10

--- 3.hs
+++ 3.hs
@@ -4,9 +4,11 @@ import Data.Numbers.Primes
 main :: IO ()
 main = print $ head $ reverse $ primeFactors 600851475143
 
+-- get a list of prime factors of a number, eg. primeFactors 12 => [2, 2, 3]
 primeFactors :: Integer -> [Integer]
 primeFactors n = primeFactors' n primes
 
+-- worker function for primeFactors
 primeFactors' :: Integer -> [Integer] -> [Integer]
 primeFactors' n (p:ps) | p > n          = []
                        | n `mod` p == 0 = p : (primeFactors' (n `div` p) (p:ps))
