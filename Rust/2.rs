34baf8b0a4291fa24179a0218b4a2a9b111eb261
Chris Anderberg <chris.anderberg@protonmail.com>
2019-01-07 23:49:27 -0800

    Use 4 space indentation in Rust

--- Rust/2.rs
+++ Rust/2.rs
@@ -1,17 +1,17 @@
 fn main() {
-  let mut sum = 0;
-  let mut prev = 0;
-  let mut curr = 1;
+    let mut sum = 0;
+    let mut prev = 0;
+    let mut curr = 1;
 
-  while curr < 4_000_000 {
-    if curr % 2 == 0 {
-      sum += curr;
+    while curr < 4_000_000 {
+        if curr % 2 == 0 {
+            sum += curr;
+        }
+
+        let next = prev + curr;
+        prev = curr;
+        curr = next;
     }
 
-    let next = prev + curr;
-    prev = curr;
-    curr = next;
-  }
-
-  println!("{}", sum);
+    println!("{}", sum);
 }
--- Rust/3.rs
+++ Rust/3.rs
@@ -1,41 +1,46 @@
 fn is_prime(primes: &Vec<u64>, num: u64) -> bool {
-  for prime in primes.iter() {
-    if num % prime == 0 {
-      return false;
-    }
-  }
+    for prime in primes.iter() {
+        if num % prime == 0 {
+            return false;
+        }
 
-  true
+        if num < prime * prime {
+            return true;
+        }
+    }
+
+    true
 }
 
 fn find_next_prime(primes: &mut Vec<u64>) -> u64 {
-  let mut potential_prime = match primes.last() {
-    Some(prime) => prime + 1,
-    None => 2,
-  };
+    let mut potential_prime = match primes.last() {
+        Some(prime) => prime + 1,
+        None => 2,
+    };
 
-  while !is_prime(primes, potential_prime) {
-    potential_prime += 1;
-  }
-  primes.push(potential_prime);
+    while !is_prime(primes, potential_prime) {
+        potential_prime += 1;
+    }
 
-  potential_prime
+    primes.push(potential_prime);
+
+    potential_prime
 }
 
 fn main() {
-  let mut primes: Vec<u64> = vec![2];
-  let mut composite: u64 = 600851475143;
-  let mut largest_prime = 2;
-  let mut cur_prime = 2;
+    let mut primes: Vec<u64> = vec![2];
+    let mut composite: u64 = 600851475143;
+    let mut largest_prime = 2;
+    let mut cur_prime = 2;
 
-  while cur_prime <= composite {
-    while composite % cur_prime == 0 {
-      composite /= cur_prime;
-      largest_prime = cur_prime;
+    while cur_prime <= composite {
+        while composite % cur_prime == 0 {
+            composite /= cur_prime;
+            largest_prime = cur_prime;
+        }
+
+        cur_prime = find_next_prime(&mut primes);
     }
 
-    cur_prime = find_next_prime(&mut primes);
-  }
-
-  println!("{}", largest_prime);
+    println!("{}", largest_prime);
 }
