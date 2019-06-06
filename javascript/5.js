112c05fc76730bd61287d599652a307c171a7760
Christopher Anderberg <chris.anderberg@tealium.com>
2019-06-06 11:52:41 -0700

    Solve Problem 8 in JavaScript.
    - Implement solution to problem 8.
    - Update eslint rules to enforce multiline comment style.
    - Update multiline comments in JavaScript solutions.

--- javascript/5.js
+++ javascript/5.js
@@ -1,15 +1,15 @@
 /*
-2520 is the smallest number that can be divided by each of
-the numbers from 1 to 10 without any remainder.
-
-What is the smallest positive number that is evenly
-divisible by all of the numbers from 1 to 20?
-*/
+ * 2520 is the smallest number that can be divided by each of
+ * the numbers from 1 to 10 without any remainder.
+ *
+ * What is the smallest positive number that is evenly
+ * divisible by all of the numbers from 1 to 20?
+ */
 
 /*
-Calculate greatest common divisor using Euclid's algorithm.
-Both arguments must be integers greater than 0.
-*/
+ * Calculate greatest common divisor using Euclid's algorithm.
+ * Both arguments must be integers greater than 0.
+ */
 function gcd(a, b) {
   if (a < b) {
     return gcd(b, a);
@@ -21,9 +21,9 @@ function gcd(a, b) {
 }
 
 /*
-Calculate least common multiple.
-Both arguments must be integers greater than 0.
-*/
+ * Calculate least common multiple.
+ * Both arguments must be integers greater than 0.
+ */
 function lcm(a, b) {
   return a * b / gcd(a, b);
 }
